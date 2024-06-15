43e05ac1c00a7cc6d107b743040183ed7843eec3
Matias Balboa <matiasbp98@gmail.com>
2024-06-15 11:35:30 -0300

    Nuevo constructor en vianda

--- src/main/java/ar/edu/utn/dds/k3003/model/Vianda.java
+++ src/main/java/ar/edu/utn/dds/k3003/model/Vianda.java
@@ -47,6 +47,13 @@ public class Vianda {
         this.estado = estado;
         this.fechaElavoracion = LocalDateTime.now();
     }
+    public Vianda(String qr,Long colaboradorId,Integer heladeraId,EstadoViandaEnum estado,LocalDateTime fechaElavoracion){
+        this.qr = qr;
+        this.colaboradorId = colaboradorId;
+        this.heladeraId = heladeraId;
+        this.estado = estado;
+        this.fechaElavoracion = fechaElavoracion;
+    }
     public Boolean esValida(Integer mes, Integer anio){
         return (this.fechaElavoracion.getYear() == anio && this.fechaElavoracion.getMonthValue() == mes);
     }
